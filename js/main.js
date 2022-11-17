cfdfa1f4e68cec497622cdc2aec8e46a3ef1aa32
c2030860 <c2030860@hallam.shu.ac.uk>
2022-11-17 16:52:52 +0000

    Images Now Cycle

--- js/main.js
+++ js/main.js
@@ -29,24 +29,44 @@
         colourBtns[i].addEventListener("click", ChangeColour);
     }
 
+    // Image Rotator
+
+    let imageAr = ['images/view1.jpg', 'images/view2.jpg', 'images/view3.jpg', 'images/view4.jpg', 'images/view5.jpg', 'images/view6.jpg'];
+
+    let count = 0;
+
+    setInterval(RotateImage, 1000);
     
 
-    // End
+    
+    
+    function ButtonPress(event){ // Event Handlers.
+        console.dir(event);
+        console.info("I was clicked.");
+    }
+
+
+    function ChangeColour(event){
+        console.dir(event.target.classList);
+
+        let bodyElement = document.querySelector("body");
+
+        correctClass = event.target.classList[0] + "Back";
+
+        bodyElement.setAttribute("class", correctClass);
+    }
+
+    // Image Rotator
+    function RotateImage(event){
+        document.getElementById("myImages").setAttribute("src", imageAr[count]);
+        
+        count += 1;
+        if (count === imageAr.length){
+            count = 0;
+        }
+        console.info("Rotate Image")
+        
+    }
+
+// End
 })()
-
-function ButtonPress (event){ // Event Handlers.
-    console.dir(event);
-    console.info("I was clicked.");
-}
-
-
-function ChangeColour(event){
-    console.dir(event.target.classList);
-    
-    let bodyElement = document.querySelector("body");
-
-    correctClass = event.target.classList[0] + "Back";
-
-    bodyElement.setAttribute("class", correctClass);
-    
-}
